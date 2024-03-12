e89f46ebb35254748ea3a14deef592a037ea6297
pedropiedade7 <142631687+pedropiedade7@users.noreply.github.com>
2024-03-12 10:38:41 -0300

    add funcs

--- index.py
+++ index.py
@@ -1,26 +1,41 @@
 import math
 
+def soma(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mult(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+def raiz(a):
+    return (round(math.sqrt(a), 2))
+
 def calculadora():
     operador = input("Informe o operador \n + - Adição \n (-) - Subtração \n * - Multiplicação \n / - Divisão \n R - Raiz ")
 
     if operador == "R":
       num1 = float(input("Informe seu numero: "))
-      resultado = round(math.sqrt(num1), 2)
+      resultado = raiz(num1)
       print("O Resultado é", resultado)
     else:
         num1 = int(input("Informe seu 1° numero: "))
         num2 = int(input("Informe seu 2° numero: "))
         if operador == "-":
-            resultado = num1 - num2
+            resultado = sub(num1, num2)
             print("O resultado é", resultado)
         elif operador == "+":
-            resultado = num1 + num2
+            resultado = soma(num1, num2)
             print("O resultado é", resultado)
         elif operador == "/":
-            resultado = num1 / num2
+            resultado = div(num1, num2)
             print("O resultado é", resultado)
         elif operador == "*":
-            resultado = num1 * num2   
+            resultado = mult(num1, num2)
             print("O resultado é", resultado)
         else:
             print("Operação inválida")
